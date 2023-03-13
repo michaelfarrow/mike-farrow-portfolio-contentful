126ab8fc0045eaca2b504180ea6538157e6844a0
Mike Farrow <mike@farrow.io>
2023-03-13 02:29:49 +0000

    trying full prism

--- src/components/general/code.tsx
+++ src/components/general/code.tsx
@@ -1,40 +1,54 @@
 import { useState, useEffect } from 'react'
 import clsx from 'clsx'
-import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+// import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 
-import arduino from 'react-syntax-highlighter/dist/cjs/languages/prism/arduino'
-import c from 'react-syntax-highlighter/dist/cjs/languages/prism/c'
-import csharp from 'react-syntax-highlighter/dist/cjs/languages/prism/csharp'
-import cpp from 'react-syntax-highlighter/dist/cjs/languages/prism/cpp'
-import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
-import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json'
-import markup from 'react-syntax-highlighter/dist/cjs/languages/prism/markup'
-import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx'
-import scss from 'react-syntax-highlighter/dist/cjs/languages/prism/scss'
-import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash'
-import shellSession from 'react-syntax-highlighter/dist/cjs/languages/prism/shell-session'
+// import arduino from 'react-syntax-highlighter/dist/cjs/languages/prism/arduino'
+// import c from 'react-syntax-highlighter/dist/cjs/languages/prism/c'
+// import csharp from 'react-syntax-highlighter/dist/cjs/languages/prism/csharp'
+// import cpp from 'react-syntax-highlighter/dist/cjs/languages/prism/cpp'
+// import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
+// import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json'
+// import markup from 'react-syntax-highlighter/dist/cjs/languages/prism/markup'
+// import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx'
+// import scss from 'react-syntax-highlighter/dist/cjs/languages/prism/scss'
+// import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash'
+// import shellSession from 'react-syntax-highlighter/dist/cjs/languages/prism/shell-session'
 
 import styles from '@/styles/components/general/code.module.css'
 
-const LANGUAGE_CONFIG = {
-  arduino,
-  c,
-  csharp,
-  cpp,
-  css,
-  markup,
-  tsx,
-  scss,
-  bash,
-  shellSession,
-  json,
-}
+// const LANGUAGE_CONFIG = {
+//   arduino,
+//   c,
+//   csharp,
+//   cpp,
+//   css,
+//   markup,
+//   tsx,
+//   scss,
+//   bash,
+//   shellSession,
+//   json,
+// }
 
-export type Language = keyof typeof LANGUAGE_CONFIG
+// export type Language = keyof typeof LANGUAGE_CONFIG
 
-Object.entries(LANGUAGE_CONFIG).forEach(([key, lang]) => {
-  SyntaxHighlighter.registerLanguage(key, lang)
-})
+export type Language =
+  | 'arduino'
+  | 'c'
+  | 'csharp'
+  | 'cpp'
+  | 'css'
+  | 'markup'
+  | 'tsx'
+  | 'scss'
+  | 'bash'
+  | 'shellSession'
+  | 'json'
+
+// Object.entries(LANGUAGE_CONFIG).forEach(([key, lang]) => {
+//   SyntaxHighlighter.registerLanguage(key, lang)
+// })
 
 interface Props {
   content: string
