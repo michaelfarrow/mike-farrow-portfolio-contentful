f0c089e85e8869289263d92c6696696bc1432675
Mike Farrow <mike@farrow.io>
2023-11-07 16:16:31 +0000

    cleaning up

--- src/components/project/attributions.tsx
+++ src/components/project/attributions.tsx
@@ -9,24 +9,22 @@ export interface Props extends React.ComponentPropsWithoutRef<'ul'> {
   entries: IProjectAttribution[]
 }
 
-export default function Experience({ className, entries, ...rest }: Props) {
+export default function Attributions({ className, entries, ...rest }: Props) {
   return (
     <ul {...rest} className={clsx(typography.textLinks, className)}>
-      {entries.map(({ fields: { shortName, attributions } }, i) => {
-        return (
-          <li key={i}>
-            {shortName} -{' '}
-            {attributions.map(({ fields: { name, link } }, i) => (
-              <Fragment key={i}>
-                {i !== 0 ? ', ' : null}
-                <OptionalLink key={i} link={link}>
-                  {name}
-                </OptionalLink>
-              </Fragment>
-            ))}
-          </li>
-        )
-      })}
+      {entries.map(({ fields: { shortName, attributions } }, i) => (
+        <li key={i}>
+          {shortName} -{' '}
+          {attributions.map(({ fields: { name, link } }, i) => (
+            <Fragment key={i}>
+              {i !== 0 ? ', ' : null}
+              <OptionalLink key={i} link={link}>
+                {name}
+              </OptionalLink>
+            </Fragment>
+          ))}
+        </li>
+      ))}
     </ul>
   )
 }
