17a5fc1b541e35d0bae835a93f409f5deee31012
Mike Farrow <mike@farrow.io>
2023-11-08 18:57:56 +0000

    enabling async components

--- src/components/content/reading-time.tsx
+++ src/components/content/reading-time.tsx
@@ -7,13 +7,12 @@ import readingTime from 'reading-time'
 import styles from '@/styles/components/content/reading-time.module.scss'
 
 interface Props extends Omit<React.ComponentPropsWithoutRef<'div'>, 'children'> {
-  content: React.ReactNode
-  children?: (props: {
-    content: React.ReactNode
-    minutesContent: React.ReactNode
-    minutes: number | null
-  }) => React.ReactNode
-
+  // content: React.ReactNode
+  // children?: (props: {
+  //   content: React.ReactNode
+  //   minutesContent: React.ReactNode
+  //   minutes: number | null
+  // }) => React.ReactNode
   wordsPerMinute?: number
 }
 
@@ -21,49 +20,50 @@ export default function ReadingTime({
   className,
   content,
   wordsPerMinute = 200,
-  children = ({ content, minutesContent }) => (
-    <>
-      {minutesContent}
-      {content}
-    </>
-  ),
-}: Props) {
-  const [minutes, setTime] = useState<number | null>(null)
-  const ref = useRef<HTMLDivElement | null>(null)
+}: // children = ({ content, minutesContent }) => (
+//   <>
+//     {minutesContent}
+//     {content}
+//   </>
+// ),
+Props) {
+  return null
+  // const [minutes, setTime] = useState<number | null>(null)
+  // const ref = useRef<HTMLDivElement | null>(null)
 
-  useEffect(() => {
-    if (ref && ref.current) {
-      setTime(
-        Math.max(
-          1,
-          Math.ceil(
-            readingTime(ref.current.innerText, {
-              wordsPerMinute,
-            }).minutes
-          )
-        )
-      )
-    }
-  }, [wordsPerMinute])
+  // useEffect(() => {
+  //   if (ref && ref.current) {
+  //     setTime(
+  //       Math.max(
+  //         1,
+  //         Math.ceil(
+  //           readingTime(ref.current.innerText, {
+  //             wordsPerMinute,
+  //           }).minutes
+  //         )
+  //       )
+  //     )
+  //   }
+  // }, [wordsPerMinute])
 
-  return (
-    <>
-      {children({
-        content: <div ref={ref}>{content}</div>,
-        minutesContent: (
-          <div
-            className={clsx(
-              styles.info,
-              minutes !== null ? styles.calculated : undefined,
-              className
-            )}
-            aria-hidden={!minutes !== null}
-          >
-            {minutes !== null ? `${minutes} minute read` : 'loading read time'}
-          </div>
-        ),
-        minutes,
-      })}
-    </>
-  )
+  // return (
+  //   <>
+  //     {children({
+  //       content: <div ref={ref}>{content}</div>,
+  //       minutesContent: (
+  //         <div
+  //           className={clsx(
+  //             styles.info,
+  //             minutes !== null ? styles.calculated : undefined,
+  //             className
+  //           )}
+  //           aria-hidden={!minutes !== null}
+  //         >
+  //           {minutes !== null ? `${minutes} minute read` : 'loading read time'}
+  //         </div>
+  //       ),
+  //       minutes,
+  //     })}
+  //   </>
+  // )
 }
