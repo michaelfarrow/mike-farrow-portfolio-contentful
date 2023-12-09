7ba27c5c36b4c6e81e4fcff7b81f71bd9a68bde2
Mike Farrow <mike@farrow.io>
2023-12-09 20:44:22 +0000

    trying generic revalidate

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -13,23 +13,26 @@ export async function POST(request: Request) {
   const topic = request.headers.get('X-Contentful-Topic')
   const body = await request.json()
 
-  if (process.env.NODE_ENV !== 'development' && secret !== process.env.CONTENTFUL_WEBHOOK_SECRET) {
-    return new Response('Invalid token', { status: 401 })
-  }
+  // if (process.env.NODE_ENV !== 'development' && secret !== process.env.CONTENTFUL_WEBHOOK_SECRET) {
+  //   return new Response('Invalid token', { status: 401 })
+  // }
 
-  if (topic?.startsWith('ContentManagement.Asset')) {
-    const id = body?.sys?.id
-    id && revalidate(tag('asset', { id }))
-    revalidate('assets')
-  }
+  // if (topic?.startsWith('ContentManagement.Asset')) {
+  //   const id = body?.sys?.id
+  //   id && revalidate(tag('asset', { id }))
+  //   revalidate('assets')
+  // }
 
-  if (topic?.startsWith('ContentManagement.Entry')) {
-    const type = body?.sys?.contentType?.sys?.id
-    const slug = body?.fields?.slug?.['en-US']
+  // if (topic?.startsWith('ContentManagement.Entry')) {
+  //   const type = body?.sys?.contentType?.sys?.id
+  //   const slug = body?.fields?.slug?.['en-US']
 
-    type && slug && revalidate(tag('entry', { type, slug }))
-    type && revalidate(tag('entries', { type }))
-  }
+  //   type && slug && revalidate(tag('entry', { type, slug }))
+  //   type && revalidate(tag('entries', { type }))
+  // }
+
+  revalidate('entry')
+  revalidate('entries')
 
   return Response.json({ ok: true })
 }
